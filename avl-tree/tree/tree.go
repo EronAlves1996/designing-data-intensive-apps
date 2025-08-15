3aec62ff8a9188cd1b27f64c8a4c8cd938e3bdf6
Eron Alves <eron.alves@rocketmail.com>
2025-08-14 23:16:46 -0300

    feat: use height to see balance

--- avl-tree/tree/tree.go
+++ avl-tree/tree/tree.go
@@ -1,17 +1,18 @@
 package tree
 
 type Node[T any] struct {
-	Value         T
-	balanceFactor int
-	Left          *Node[T]
-	Right         *Node[T]
-	compare       func(T, T) int
+	Value   T
+	height  int
+	Left    *Node[T]
+	Right   *Node[T]
+	compare func(T, T) int
 }
 
 func NewNode[T any](value T, compare func(T, T) int) Node[T] {
 	return Node[T]{
 		Value:   value,
 		compare: compare,
+		height:  1,
 	}
 }
 
@@ -20,7 +21,39 @@ func rotateLeft[T any](n *Node[T]) *Node[T] {
 	child := actual.Right
 	actual.Right = nil
 	child.Left = actual
+	actual.height = height(actual)
+	child.height = height(child)
 	return child
+}
+
+func rotateRight[T any](n *Node[T]) *Node[T] {
+	actual := n
+	child := actual.Left
+	actual.Left = nil
+	child.Right = actual
+	actual.height = height(actual)
+	child.height = height(child)
+	return child
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func rotate[T any](n *Node[T]) *Node[T] {
+	diff := (height(n.Right) - height(n.Left))
+	if diff > 1 {
+		r := rotateLeft(n)
+		return r
+	}
+	if diff < -1 {
+		r := rotateRight(n)
+		return r
+	}
+	return n
 }
 
 func Insert[T any](n *Node[T], v T) *Node[T] {
@@ -28,44 +61,46 @@ func Insert[T any](n *Node[T], v T) *Node[T] {
 		if n.Left == nil {
 			nn := NewNode(v, n.compare)
 			n.Left = &nn
-			n.balanceFactor = -1
+			n.height = height(n)
 			return n
 		}
 
-		Insert(n.Left, v)
-		n.balanceFactor += n.Left.balanceFactor
-		return n
+		n.Left = Insert(n.Left, v)
+		n.height = height(n)
+		return rotate(n)
 	}
 
 	if n.Right == nil {
 		nn := NewNode(v, n.compare)
 		n.Right = &nn
-		n.balanceFactor += 1
+		n.height = height(n)
 		return n
 	}
 
-	Insert(n.Right, v)
-	n.balanceFactor += n.Right.balanceFactor
-	if n.balanceFactor > 1 {
-		r := rotateLeft(n)
-		r.balanceFactor = reviewBalance(r)
-		return r
-	}
-	return n
+	n.Right = Insert(n.Right, v)
+	n.height = height(n)
+
+	return rotate(n)
 }
 
-func reviewBalance[T any](r *Node[T]) int {
-	le := 0
-	ri := 0
-	if r.Right != nil {
-		ri = reviewBalance(r.Right) + 1
+func height[T any](n *Node[T]) int {
+	if n == nil {
+		return 0
 	}
 
-	if r.Left != nil {
-		le = reviewBalance(r.Left) + 1
+	if n.Left == nil && n.Right == nil {
+		return 1
 	}
 
-	return ri - le
+	if n.Right == nil {
+		return n.Left.height + 1
+	}
+
+	if n.Left == nil {
+		return n.Right.height + 1
+	}
+
+	return max(n.Right.height, n.Left.height) + 1
 }
 
 func (n *Node[T]) Ordered() []T {
--- avl-tree/tree/tree_test.go
+++ avl-tree/tree/tree_test.go
@@ -11,36 +11,193 @@ func createBaseCase() Node[int] {
 func TestInsertSequential(t *testing.T) {
 	n := createBaseCase()
 	a := &n
-	AssertEquals(a.balanceFactor, 0, t)
+	if a.height != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.height)
+	}
 
 	a = Insert(a, 2)
-	AssertEquals(a.balanceFactor, 1, t)
+
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
 
 	a = Insert(a, 3)
-	AssertEquals(a.balanceFactor, 0, t)
 
-	AssertEquals(a.Value, 2, t)
-	AssertEquals(a.Right.Value, 3, t)
-	AssertEquals(a.Left.Value, 1, t)
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
+
+	if a.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Value)
+	}
+
+	if a.Right.Value != 3 {
+		t.Fatalf("Expect %d found %d", 3, a.Right.Value)
+	}
+
+	if a.Left.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Left.Value)
+	}
 }
 
-func AssertEquals(a, b int, t *testing.T) {
-	if a != b {
-		t.Fatalf("Want %d, found %d", b, a)
+func TestInsertSequentialBackwards(t *testing.T) {
+	n := createBaseCase()
+	a := &n
+
+	if a.height != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.height)
+	}
+
+	a = Insert(a, 0)
+
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
+
+	a = Insert(a, -1)
+
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
+
+	expect := 0
+	if a.Value != expect {
+		t.Fatalf("Expect %d found %d", expect, a.Value)
+	}
+
+	expect = 1
+	if a.Right.Value != expect {
+		t.Fatalf("Expect %d found %d", expect, a.Right.Value)
+	}
+
+	expect = -1
+	if a.Left.Value != expect {
+		t.Fatalf("Expect %d found %d", expect, a.Left.Value)
+	}
+}
+
+func TestInsertSequentialTwoLevels(t *testing.T) {
+	n := createBaseCase()
+	a := &n
+	if a.height != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.height)
+	}
+
+	a = Insert(a, 2)
+
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
+
+	a = Insert(a, 3)
+
+	if a.height != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.height)
+	}
+
+	a = Insert(a, 4)
+
+	if a.height != 3 {
+		t.Fatalf("Expect %d found %d", 3, a.height)
+	}
+
+	if a.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Value)
+	}
+
+	if a.Right.Value != 3 {
+		t.Fatalf("Expect %d found %d", 3, a.Right.Value)
+	}
+
+	if a.Right.Right.Value != 4 {
+		t.Fatalf("Expect %d found %d", 4, a.Right.Value)
+	}
+
+	if a.Left.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Left.Value)
+	}
+}
+
+func TestInsertNotSequential(t *testing.T) {
+	n := createBaseCase()
+	a := &n
+	a = Insert(a, 2)
+
+	if a.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Value)
+	}
+
+	if a.Right.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Right.Value)
+	}
+
+	a = Insert(a, -2)
+
+	if a.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Value)
+	}
+
+	if a.Right.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Right.Value)
+	}
+
+	if a.Left.Value != -2 {
+		t.Fatalf("Expect %d found %d", -2, a.Right.Value)
+	}
+
+	a = Insert(a, -5)
+
+	if a.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Value)
+	}
+
+	if a.Right.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Right.Value)
+	}
+
+	if a.Left.Value != -2 {
+		t.Fatalf("Expect %d found %d", -2, a.Right.Value)
+	}
+
+	if a.Left.Left.Value != -5 {
+		t.Fatalf("Expect %d found %d", -5, a.Right.Value)
+	}
+
+	a = Insert(a, -6)
+
+	if a.Value != 1 {
+		t.Fatalf("Expect %d found %d", 1, a.Value)
+	}
+
+	if a.Left.Value != -5 {
+		t.Fatalf("Expect %d found %d", -5, a.Left.Value)
+	}
+
+	if a.Left.Left.Value != -6 {
+		t.Fatalf("Expect %d found %d", -6, a.Left.Left.Value)
+	}
+
+	if a.Left.Right.Value != -2 {
+		t.Fatalf("Expect %d found %d", -2, a.Left.Right.Value)
+	}
+
+	if a.Right.Value != 2 {
+		t.Fatalf("Expect %d found %d", 2, a.Right.Value)
 	}
 }
 
 // func TestInsertBalanced(t *testing.T) {
 // 	n := createBaseCase()
-// 	AssertEquals(n.balanceFactor, 0, t)
+// 	r := &n
+// 	AssertEquals(r.balanceFactor, 0, t)
 
-// 	n.Insert(-1)
-// 	AssertEquals(n.balanceFactor, -1, t)
+// 	r = Insert(r, -1)
+// 	AssertEquals(r.balanceFactor, -1, t)
 
-// 	n.Insert(2)
-// 	AssertEquals(n.balanceFactor, 0, t)
+// 	r = Insert(r, 2)
+// 	AssertEquals(r.balanceFactor, 0, t)
 
-// 	AssertEquals(n.Value, 1, t)
-// 	AssertEquals(n.Left.Value, -1, t)
-// 	AssertEquals(n.Right.Value, 2, t)
+// 	AssertEquals(r.Value, 1, t)
+// 	AssertEquals(r.Left.Value, -1, t)
+// 	AssertEquals(r.Right.Value, 2, t)
 // }
