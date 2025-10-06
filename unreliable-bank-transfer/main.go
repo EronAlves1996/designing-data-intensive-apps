e752d07fb2fa06560962428e1e8604356a0a6688
Eron Alves <eron.alves@rocketmail.com>
2025-10-06 17:03:34 -0300

    feat: implement the unreliable function

--- unreliable-bank-transfer/main.go
+++ unreliable-bank-transfer/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
 
 type BankAccount struct {
 	accountID int
-	balance   big.Float
+	balance   *big.Float
 }
 
 var bankAccounts = []BankAccount{}
+
+func transferFundsNonAtomic(fromAccount BankAccount, toAccount BankAccount, amount big.Float) error {
+	fromAccount.balance = new(big.Float).SetPrec(64).Sub(fromAccount.balance, &amount)
+	return fmt.Errorf("database node crashed")
+	toAccount.balance = new(big.Float).SetPrec(64).Add(toAccount.balance, &amount)
+	return nil
+}
