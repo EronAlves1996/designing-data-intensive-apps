a4ea19981d748f14614c0808b1e69bf02bd0970c
Eron Alves <eron.alves@rocketmail.com>
2025-10-06 17:12:53 -0300

    feat: create two new functions

--- unreliable-bank-transfer/main.go
+++ unreliable-bank-transfer/main.go
@@ -10,13 +10,21 @@ type BankAccount struct {
 	balance   *big.Float
 }
 
-var bankAccounts = []BankAccount{}
-
 func transferFundsNonAtomic(fromAccount *BankAccount, toAccount *BankAccount, amount big.Float) error {
 	fromAccount.balance = new(big.Float).SetPrec(64).Sub(fromAccount.balance, &amount)
 	return fmt.Errorf("database node crashed")
 	toAccount.balance = new(big.Float).SetPrec(64).Add(toAccount.balance, &amount)
 	return nil
+}
+
+func transferFunds(fromAccount *BankAccount, toAccount *BankAccount, amount big.Float) error {
+	fromAccount.balance = new(big.Float).SetPrec(64).Sub(fromAccount.balance, &amount)
+	toAccount.balance = new(big.Float).SetPrec(64).Add(toAccount.balance, &amount)
+	return nil
+}
+
+func getTotalBalance(a *BankAccount, b *BankAccount) *big.Float {
+	return new(big.Float).SetPrec(64).Add(a.balance, b.balance)
 }
 
 func main() {
